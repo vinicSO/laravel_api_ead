53d2cf7c7f5393744b4953894f1f764b20a6636a
Vinícius S Oliveira <vinicius.santos.oliv3ira@gmail.com>
2023-03-02 17:01:35 -0300

    work with uuid

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+Use App\Models\Traits\UuidTrait;
 
 class User extends Authenticatable
 {
