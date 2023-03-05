a788d346cc0042d586abbfc3a4ebca59a3dd78a0
Vinicius S Oliveira - Personal <vinicius.santos.oliv3ira@gmail.com>
2023-03-05 20:38:30 -0300

    auth tests

---
+++ tests/Feature/Api/Auth/AuthTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Tests\Feature\Api\Auth;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthTest extends TestCase
+{
+    
+    public function test_fail_auth() {
+
+        $response = $this->postJson('/auth');
+
+        $response->assertStatus(422);
+    }
+
+    public function test_auth() {
+
+        $user = User::factory()->create();
+
+        $response = $this->postJson('/auth', [
+            'email' => $user->email,
+            'password' => 'password',
+            'device_name' => 'teste-dvc'
+        ]);
+
+        // $response->dump(); // Return the response body
+
+        $response->assertStatus(200);
+    }
+
+    public function test_fail_logout () {
+
+        $response = $this->postJson('/logout');
+
+        $response->assertStatus(401);
+    }
+
+    public function test_logout () {
+
+        $user = User::factory()->create();
+        $token = $user->createToken('teste')->plainTextToken;
+
+        $response = $this->postJson('/logout', [], [
+            'Authorization' => 'Bearer ' . $token
+        ]);
+
+        $response->dump(); // Return the response body
+
+        $response->assertStatus(200);
+    }
+
+    public function test_fail_get_me () {
+
+        $response = $this->postJson('/me');
+
+        $response->assertStatus(401);
+    }
+
+    public function test_get_me () {
+
+        $user = User::factory()->create();
+        $token = $user->createToken('teste')->plainTextToken;
+
+        $response = $this->postJson('/me', [], [
+            'Authorization' => 'Bearer ' . $token
+        ]);
+
+        $response->assertStatus(200);
+    }
+}
