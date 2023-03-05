9159d259cbd5ac670b3f18fce52dd2ba48992246
Vinicius S Oliveira - Personal <vinicius.santos.oliv3ira@gmail.com>
2023-03-05 11:53:46 -0300

    use middleware of authorization

--- routes/api.php
+++ routes/api.php
@@ -10,20 +10,25 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/auth', [AuthController::class, 'auth']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware(['auth:sanctum']);
 
-Route::get('/courses/{id}', [CourseController::class, 'find']);
-Route::get('/courses', [CourseController::class, 'index']);
+Route::middleware(['auth:sanctum'])->group( function () {
+    Route::get('/courses/{id}', [CourseController::class, 'find']);
+    Route::get('/courses', [CourseController::class, 'index']);
 
-Route::get('/courses/{id}/modules', [ModuleController::class, 'index']);
+    Route::get('/courses/{id}/modules', [ModuleController::class, 'index']);
 
-Route::get('/modules/{id}/lessons', [LessonController::class, 'index']);
-Route::get('/lessons/{id}', [LessonController::class, 'show']);
+    Route::get('/modules/{id}/lessons', [LessonController::class, 'index']);
+    Route::get('/lessons/{id}', [LessonController::class, 'show']);
 
-Route::get('/supports', [SupportController::class, 'index']);
-Route::get('/supports/my', [SupportController::class, 'mySupports']);
-Route::post('/supports', [SupportController::class, 'store']);
+    Route::get('/supports', [SupportController::class, 'index']);
+    Route::get('/supports/my', [SupportController::class, 'mySupports']);
+    Route::post('/supports', [SupportController::class, 'store']);
 
-Route::post('/replies', [ReplySupportController::class, 'createReply']);
+    Route::post('/replies', [ReplySupportController::class, 'createReply']);
+});
+
+
 
 Route::get('/', function () {
     return response()->json([
