e3878c55ac525ea30d063384d852c53b63637752
root <root@DESKTOP-Q7ND8RG>
2023-03-06 09:20:23 -0300

    other supports tests

--- tests/Feature/Api/SupportTest.php
+++ tests/Feature/Api/SupportTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Api;
 
+use App\Models\Lesson;
 use App\Models\Support;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -38,5 +39,42 @@ class SupportTest extends TestCase
 
         $response->assertStatus(200)
             ->assertJsonCount(50, 'data');
+    }
+
+    public function test_get_supports_unauthenticated ()
+    {
+
+        $response = $this->getJson('/supports', $this->defaultUnauthorizedHeaders());
+
+        $response->assertStatus(401);
+    }
+
+    public function test_get_supports ()
+    {
+
+        Support::factory()->count(50)->create();
+
+        $response = $this->getJson('/supports', $this->defaultAuthorizedHeaders());
+
+        $response->assertStatus(200)
+            ->assertJsonCount(50, 'data');
+    }
+
+    public function test_get_supports_filtered_by_lesson ()
+    {
+
+        $lesson = Lesson::factory()->create();
+
+        Support::factory()->count(50)->create();
+        Support::factory()->count(10)->create([
+            'lesson_id' => $lesson->id
+        ]);
+
+        $response = $this->json('GET', '/supports', [
+            'lesson' => $lesson->id
+        ], $this->defaultAuthorizedHeaders());
+
+        $response->assertStatus(200)
+            ->assertJsonCount(10, 'data');
     }
 }
